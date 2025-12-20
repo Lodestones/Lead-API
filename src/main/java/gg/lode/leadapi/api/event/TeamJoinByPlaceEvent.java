e7d89b00434e68e7d997bff53a6c14a4c263558f
John Aquino <41652412+apollyon600@users.noreply.github.com>
2025-12-19 18:09:44 -0700

    feat: add TeamJoinByPlaceEvent

---
+++ src/main/java/gg/lode/leadapi/api/event/TeamJoinByPlaceEvent.java
@@ -0,0 +1,37 @@
+package gg.lode.leadapi.api.event;
+
+import gg.lode.bookshelfapi.api.event.BaseEvent;
+import gg.lode.leadapi.api.ITeam;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Cancellable;
+
+public class TeamJoinByPlaceEvent extends BaseEvent implements Cancellable {
+
+    private final Player player;
+    private final ITeam team;
+
+    private boolean isCancelled;
+
+    public TeamJoinByPlaceEvent(ITeam team, Player player) {
+        this.player = player;
+        this.team = team;
+    }
+
+    @Override
+    public boolean isCancelled() {
+        return isCancelled;
+    }
+
+    @Override
+    public void setCancelled(boolean b) {
+        this.isCancelled = b;
+    }
+
+    public ITeam getTeam() {
+        return team;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+}
